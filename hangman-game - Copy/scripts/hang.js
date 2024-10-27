6febcb983d96ba07fe5335f5d1b3418865f59741
Christine Nammoura <christinejnammoura@gmail.com>
2024-10-27 10:27:53 +0200

    Fifth commit: update hangman

--- hangman-game - Copy/scripts/hang.js
+++ hangman-game - Copy/scripts/hang.js
@@ -1,23 +1,5 @@
 
 
-var words = ["JAVASCRIPT", "HTML", "CSS", "PROGRAMMING", "DEVELOPER", "FUNCTION", "VARIABLE", "OBJECT"];
-
-
-var selectedWord = ""; 
-var guessedLetters = []; 
-var wrongGuesses = 0; 
-var maxWrongGuesses = 6; 
-
-
-function startGame() {
-    selectedWord = words[Math.floor(Math.random() * words.length)]; 
-    guessedLetters = []; 
-    wrongGuesses = 0; 
-    updateDisplay(); 
-}
-
-
-
 
 function updateDisplay() {
     
@@ -52,26 +34,6 @@ function updateDisplay() {
     
     updateHangman();
 }
-
-
-function addLetterClickListeners() {
-    
-    var letterDivs = document.querySelectorAll('.letter');
-
-    
-    for (var i = 0; i < letterDivs.length; i++) {
-        
-        letterDivs[i].addEventListener('click', function() {
-            
-            var clickedLetter = this.textContent; 
-            
-            
-            handleLetterClick(clickedLetter);
-        });
-    }
-}
-addLetterClickListeners();
-
 
 
 
